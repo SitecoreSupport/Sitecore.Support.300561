d042c1722b0e43fa719cfe4618a4f550cd978f1e
Philippe Fillion <pfi@sitecore.net>
2018-12-20 15:00:09 -0500

    Initial commit

---
+++ src/Sitecore.Support.300561/sitecore/shell/Controls/Rich Text Editor/Dialog.js
@@ -0,0 +1,14 @@
+﻿if (Sys && Sys.Application) {
+    Sys.Application.add_init(function () {
+        if (Telerik && Telerik.Web && Telerik.Web.UI.Widgets && Telerik.Web.UI.Widgets.ImageProperties) {
+            Telerik.Web.UI.Widgets.ImageProperties.prototype._setClass = function (element, cssClassHolder) {
+                if (cssClassHolder.get_value() === "") {
+                    element.removeAttribute("class");
+                }
+                else {
+                    element.className = cssClassHolder.get_value();
+                }
+            }
+        }
+    });
+}
